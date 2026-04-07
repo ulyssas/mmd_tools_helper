51cb78e7cc7193c1ce588540d76f5d56500d2a91
WimPum <n1v0x.5127e@gmail.com>
2026-04-07 11:48:47 +0900

    fix versioning, re-release v4.3.0

--- mmd_tools_helper/translations.py
+++ mmd_tools_helper/translations.py
@@ -13,7 +13,7 @@ translations_tuple = (
     (
         ("*", ""),
         ((), ()),
-        ("ja_JP", "MMD Tools Helper 4.3 (0)", (False, ("FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.",))),
+        ("ja_JP", "MMD Tools Helper 4.3.0 (0)", (False, ("FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.",))),
     ),
     (
         ("Operator", "Add arm IK to MMD model"),
